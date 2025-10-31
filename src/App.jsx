20dc8736be84e75f27b626b340de0828e60c8cf3
Niruth Ananth <niruthananth03@gmail.com>
2025-10-31 12:58:00 +0530

    login/signup UI design made

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import {Routes,Route} from "react-router-dom"
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import AdminLogin from "./admin/components/Login.jsx";
+import Login from "./client/components/Login";
+import Signup from "./client/components/Signup.jsx";
+
 const App = () => {
   return (
     <Routes>
-  
-      <Route path='admin'/>
+      <Route path="/admin" element={<AdminLogin />} />
+      <Route path='/login' element={<Login/>} />
+      <Route path='/signup' element={<Signup/>} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
